39fe9c8ce4fc874780e6613f63d197405aeed1b3
odeifuo <quasiahama@gmail.com>
2024-10-19 23:20:15 +0000

    edited landingpage

---
+++ src/pages/landingpage/GridView.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../component/Navbar'
+
+const GridView = () => {
+  return (
+    <div>
+        
+        <Navbar />
+    </div>
+  )
+}
+
+export default GridView
---
+++ src/pages/landingpage/ListView.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../../component/Navbar' 
+import Hero from '../../pages/landingpage/index'
+
+const ListView = () => {
+  return ( 
+
+   
+    <div> 
+        <Navbar /> 
+        <Hero />
+    
+    </div>
+  )
+}
+
+export default ListView
+
