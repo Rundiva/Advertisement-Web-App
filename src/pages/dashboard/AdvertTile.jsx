81065f239f17e1bc279516ee87fda2b308f54841
Faiza Abdul-Wahab <faizaabdulwahab@ymail.com>
2024-10-24 18:49:02 +0000

    shaffle worked on

--- src/pages/dashboard/AdvertTile.jsx
+++ src/pages/dashboard/AdvertTile.jsx
@@ -3,13 +3,14 @@ import { Link } from "react-router-dom";
 const AdvertTile = ({ id, title, description, image, category, price}) => {
     return(
         <div className="flex flex-col">
-            <Link to={`/adverts/${id}`} className=" border-[10px]">
+            <Link to={`/adverts/${id}`} className=" border-[3px] w-[18vw] p-4 ">
             {/* <input type="checkbox" /> */}
-            <p>{title}</p>
-            <p>{description}</p>
-            <p>{category}</p>
-            <p>{price}</p>
+            
             <img src={`https://savefiles.org/${image}?shareable_link=450`} alt={title}/>
+            <h1 className="pt-1 pb-1 font-bold">{title}</h1>
+            <p className="pb-1">{description}</p>
+            <p className="pb-1">{category}</p>
+            <p className="pb-1">{price}</p>
         </Link>
         </div>
     )
